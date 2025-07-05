cd435423cad7dec8ec3dc8c3f18e41b245a758f3
charu-chandak-aaft <charu.c@aaft.com>
2025-07-05 14:59:48 +0530

    fix-for-date-after-qa

--- src/app/components/shared/HeaderBar.jsx
+++ src/app/components/shared/HeaderBar.jsx
@@ -67,7 +67,7 @@ export default function HeaderBar({profile}) {
         {/* Country Flag */}
         <div className="bg-[#1F2A40] p-1.5 rounded-md cursor-pointer">
           <Image
-            src="/logos/indian-flag.jpg"
+            src="/logos/indianflag.jpg"
             alt="India-Flag"
             width={24}
             height={24}
